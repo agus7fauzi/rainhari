fccb56b0ae17dcaf17577a111702079161be7d17
Agus Imam Fauzi <agus7fauzi@gmail.com>
2022-04-17 01:36:14 +0700

    develop protoc-gen-rainhari tool

---
+++ cmd/protoc-gen-rainhari/main.go
@@ -0,0 +1,34 @@
+/*
+Copyright 2022 Agus Imam Fauzi <agus7fauzi@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"github.com/agus7fauzi/rainhari/cmd/protoc-gen-rainhari/plugin"
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+func main() {
+	protogen.Options{}.Run(func(gen *protogen.Plugin) error {
+		for _, f := range gen.Files {
+			if !f.Generate {
+				continue
+			}
+			plugin.GenerateFile(gen, f)
+		}
+		return nil
+	})
+}
